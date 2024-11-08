88b8044d0a5474bb0b5af956796f2141584ab748
Rocket <Rocket@4399.com>
2024-11-08 16:16:23 +0800

    first commit

---
+++ src/main/java/com/git_learn/git_test.java
@@ -0,0 +1,8 @@
+package com.git_learn;
+
+public class git_test {
+    public static void main(String[] args) {
+      System.out.println("hello world");
+      System.out.println("hello world");
+    }
+}
